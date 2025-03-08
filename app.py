e434b775553fdcb76e14e9317c136dbd7f6a8c68
chhaya1 <chhayasharma1401@gmail.com>
2025-03-08 19:29:21 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
+# Load envvarae
 load_dotenv()
 
 # Initialize Flask app
